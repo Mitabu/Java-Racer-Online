0176dfda409f562508af0f42e6e36b19fb7fdd76
Mitabu <polkovnikovartem30@gmail.com>
2021-03-29 17:17:52 +0200

    Create GameServer.java
    Test Push

---
+++ GameServer.java
@@ -0,0 +1,27 @@
+// JavaFX
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+import javafx.scene.image.*;
+import javafx.scene.shape.*;
+import javafx.animation.AnimationTimer;// Animation timer
+import javafx.scene.input.KeyEvent;// Key Listener
+// Java
+import java.io.*;
+import java.util.*;
+
+/**
+*  @author Artem Polkovnikov
+*  @version 29.03.2021
+*/
+
+public class GameServer extends Application implements EventHandler<ActionEvent>
+{
+   
+}
