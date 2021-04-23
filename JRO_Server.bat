32b20ba259a60fa2d792d09f7228d09ebf4205b0
Mitabu <polkovnikovartem30@gmail.com>
2021-04-23 13:45:05 +0200

    Changed .bat to start the game without console

--- JRO_Server.bat
+++ JRO_Server.bat
@@ -1,1 +1,1 @@
-java --module-path Addons\JavaFX\javafx-sdk-11.0.2\lib --add-modules=javafx.controls -jar Assets\GameServer.jar
+start javaw --module-path Addons\JavaFX\javafx-sdk-11.0.2\lib --add-modules=javafx.controls -jar Assets\GameServer.jar
